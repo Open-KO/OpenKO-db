2698762d3fb632bd01921579d7351a84287b5047
kenner2 <13474249+kenner2@users.noreply.github.com>
2025-07-11 14:56:41 -0400

    add Atoi function for DbType

--- jsonSchema/enums/dbType/dbType.go
+++ jsonSchema/enums/dbType/dbType.go
@@ -7,3 +7,16 @@ const (
 	GAME    DbType = "GAME"
 	LOG     DbType = "LOG"
 )
+
+func (d DbType) Atoi() int8 {
+	switch d {
+	case ACCOUNT:
+		return 0
+	case GAME:
+		return 1
+	case LOG:
+		return 2
+	default:
+		return -1
+	}
+}
